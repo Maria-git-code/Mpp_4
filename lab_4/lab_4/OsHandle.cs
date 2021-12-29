7ff0ac26eef775ef19e33de8c03b51406d8b9089
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:46:17 +0300

    Add files via upload

---
+++ lab_4/lab_4/OsHandle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab_4
+{
+    public class OsHandle : IDisposable
+    {
+        private IntPtr _handle;
+        private bool _disposed;
+        
+        public IntPtr Handle 
+        {
+            get
+            {
+                if (!_disposed)
+                {
+                    return _handle;
+                }
+                else
+                {
+                    throw new ObjectDisposedException(ToString());
+                }
+            }
+            set { _handle = value; }
+        }
+        [DllImport("Kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr handle);
+        
+        public OsHandle()
+        {
+            _handle = IntPtr.Zero;
+        }
+        
+        ~OsHandle()
+        {
+            Dispose(false);
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+                
+        public bool Close()
+        {
+            var result = CloseHandle(Handle);
+            Dispose();
+            return result;
+        }
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && _handle != IntPtr.Zero)
+            {
+                CloseHandle(Handle);
+                _handle = IntPtr.Zero;
+            }
+
+            _disposed = true;
+        }
+    }
+}
---
+++ lab_4/lab_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int handleNumber;
+            Console.WriteLine("Введите номер дескриптора ОС: ");
+            while (!int.TryParse(Console.ReadLine(), NumberStyles.HexNumber, CultureInfo.InvariantCulture,
+                out handleNumber))
+                Console.WriteLine("Ошибка ввода, введите номер дескриптора ОС (формат 1b3): ");
+            var osHandle = new OsHandle {Handle = new IntPtr(handleNumber)};
+            Console.WriteLine(osHandle.Close() ? "Дескриптор удалось закрыть" : "Дескриптор не удалось закрыть");
+            osHandle.Dispose();
+        }
+    }
+}
